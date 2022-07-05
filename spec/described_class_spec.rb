fa1d466b10c21c8f6883d08326eac328f5356a06
Wong Kok Wah <TIMWKW@fwd.com>
2022-07-05 16:29:57 +0800

    shorthand rspec syntax

---
+++ spec/described_class_spec.rb
@@ -0,0 +1,18 @@
+class King
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+RSpec.describe King do
+  subject { described_class.new('Boris') } 
+  let(:louis) { described_class.new 'Louis' } 
+
+  it 'represent some person' do 
+    expect(subject.name).to eq('Boris')
+    expect(louis.name).to eq('Louis')
+  end
+end
